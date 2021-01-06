bdbd3c69da8558a49ede87a48210e4a984ff00c0
Sok Den <59393496+Sok-Den@users.noreply.github.com>
2021-01-07 00:04:35 +0700

    add lesson 2

---
+++ lesson_2.py
@@ -0,0 +1,10 @@
+character_name = "George"
+character_age = "70"
+
+print("There once was a man named " + character_name)
+print("He was " + character_age + " years old")
+
+character_age = "80"
+
+print("He really liked the name " + character_name)
+print("But didn't like being " + character_age)
