a6fd245a3aeedc118ff9f1a3fdf491f2ca8c04da
sokdenreallife <sokdenreallife@gmail.com>
2021-01-16 14:42:04 +0700

    add input

---
+++ input.py
@@ -0,0 +1,13 @@
+# Create a calculation
+calNumber1 = input("Enter number: ")
+calNumber2 = input("Enter another number: ")
+
+plusA = calNumber1 + calNumber2
+plusB = calNumber1 + calNumber2
+plusC = calNumber1 + calNumber2
+plusD = calNumber1 + calNumber2
+
+print(type(plusA))
+print(plusB)
+print(plusC)
+print(plusD)
